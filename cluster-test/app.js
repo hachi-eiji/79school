ea610c0ff03a4eb5d241f5e596273c2b955007ff
hachi-eiji <hachiyae@gmail.com>
2014-12-10 00:31:33 +0900

    implement Cluster2
    TODO: how to debug...

--- cluster-test/app.js
+++ cluster-test/app.js
@@ -1,87 +1,75 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// cluster module
+var Cluster2 = require('cluster2');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-// cluster module
-var numCPUs = require('os').cpus().length;
-var cluster = require('cluster');
+var app = express();
 
-if (cluster.isMaster) {
-  console.log('fork %s workers from master', numCPUs);
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on('online', function(worker) {
-    console.log('worker is running on %s', worker.process.pid);
-  });
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker with %s is closed', worker.process.pid);
-  });
-} else if (cluster.isWorker) {
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  var app = express();
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+app.use('/', routes);
+app.use('/users', users);
+app.use('/check', function (req, res) {
+  res.send(200, 'hello');
+});
 
-  // uncomment after placing your favicon in /public
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  app.use('/', routes);
-  app.use('/users', users);
-  app.use('/check', function(req,res){
-    res.send(200, 'cluster ' + cluster.worker.process.pid +' responded \n');
-  });
+// error handlers
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  // error handlers
-
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
-  }
-
-  // production error handler
-  // no stacktraces leaked to user
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: err
     });
   });
-
-  var port = 3000;
-  console.log('worker (%s) is now listening to http://localhost:%s', cluster.worker.process.pid, port);
-  app.listen(port);
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var port = 3000;
+var cluster2 = new Cluster2({
+  port: port
+});
+cluster2.listen(function (callback) {
+  callback(app);
+});
 
 //var server = http.createServer(app);
 //server.listen(3000, function(){

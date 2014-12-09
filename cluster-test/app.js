9f3a0e50eba45833a207ff47bd4f427fe1626c27
hachi-eiji <hachiyae@gmail.com>
2014-12-09 23:50:46 +0900

    add cluster module

--- cluster-test/app.js
+++ cluster-test/app.js
@@ -5,62 +5,87 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+// cluster module
+var numCPUs = require('os').cpus().length;
+var cluster = require('cluster');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+if (cluster.isMaster) {
+  console.log('fork %s workers from master', numCPUs);
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', function(worker) {
+    console.log('worker is running on %s', worker.process.pid);
+  });
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker with %s is closed', worker.process.pid);
+  });
+} else if (cluster.isWorker) {
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  var app = express();
 
-app.use('/', routes);
-app.use('/users', users);
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use('/', routes);
+  app.use('/users', users);
+  app.use('/check', function(req,res){
+    res.send(200, 'cluster ' + cluster.worker.process.pid +' responded \n');
+  });
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
+  });
 
-// error handlers
+  // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
     });
-}
+  }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+      message: err.message,
+      error: {}
     });
-});
+  });
 
-var server = http.createServer(app);
-server.listen(3000, function(){
-  console.log('start server...');
-});
+  var port = 3000;
+  console.log('worker (%s) is now listening to http://localhost:%s', cluster.worker.process.pid, port);
+  app.listen(port);
+}
+
+//var server = http.createServer(app);
+//server.listen(3000, function(){
+//  console.log('start server...');
+//});
 
 // module.exports = app;
-
